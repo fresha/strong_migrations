626f183666f5c2c7b1ef1acc454dc8b0100aa258
Patryk Woziński <patryk.wozinski@gmail.com>
2021-10-28 21:27:09 +0200

    test for reasons_translator module

---
+++ test/unit/strong_migrations/reasons_translator_test.exs
@@ -0,0 +1,28 @@
+defmodule StrongMigrations.ReasonsTranslatorTest do
+  use ExUnit.Case, async: true
+
+  alias StrongMigrations.ReasonsTranslator
+
+  test "translates to safe when no specified reasons for migrations" do
+    to_translate = [
+      {"migration.exs", []}
+    ]
+
+    assert :safe == ReasonsTranslator.translate(to_translate)
+  end
+
+  test "translates unsafe when migrations with reasons specified" do
+    to_translate = [
+      {"migration.exs", [:i_like_trains, :elixir_is_cool]}
+    ]
+
+    {status, reasons} = ReasonsTranslator.translate(to_translate)
+
+    assert :unsafe == status
+
+    assert [
+             "Migration migration.exs is not safe! Reason: i like trains",
+             "Migration migration.exs is not safe! Reason: elixir is cool"
+           ] == reasons
+  end
+end
