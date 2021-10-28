605c19d5f736e543398e250626b46d640e106ac7
Patryk Woziński <patryk.wozinski@gmail.com>
2021-10-28 12:39:10 +0200

    docs and required deps

--- mix.exs
+++ mix.exs
@@ -1,28 +1,50 @@
 defmodule StrongMigrations.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/patrykwozinski/strong_migrations"
+  @version "0.1.0"
+
   def project do
     [
       app: :strong_migrations,
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      dialyzer: [
+        plt_add_deps: :apps_direct,
+        plt_add_apps: [:mix],
+        plt_ignore_apps: [:logger]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.25.5", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        # "HOW_TO_RELEASE.md": [title: "How to Release"],
+        # "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
