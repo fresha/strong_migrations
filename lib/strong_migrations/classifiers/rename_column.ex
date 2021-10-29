7dc2572e4fe1deac44f20e8d7a6f3085dd7c77b1
Patryk Woziński <patryk.wozinski@gmail.com>
2021-10-29 09:36:37 +0200

    rename column can be safety assured

--- lib/strong_migrations/classifiers/rename_column.ex
+++ lib/strong_migrations/classifiers/rename_column.ex
@@ -6,11 +6,15 @@ defmodule StrongMigrations.Classifiers.RenameColumn do
   @behaviour Classifier
 
   @impl Classifier
-  def classify(%{rename_column: true}), do: failed()
+  def classify(%{rename_column: true, safety_assured: safety_assured}) do
+    is_safe(:rename_column in safety_assured)
+  end
 
+  def classify(%{rename_column: true}), do: failed()
   def classify(_migration), do: :ok
 
-  defp failed do
-    {:error, :rename_column_is_not_safe}
-  end
+  defp is_safe(true), do: :ok
+  defp is_safe(false), do: failed()
+
+  defp failed, do: {:error, :rename_column_is_not_safe}
 end
