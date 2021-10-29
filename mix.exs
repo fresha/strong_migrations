e29472eb9af05144374cb001067848c63112a1d7
Patryk Woziński <patryk.wozinski@gmail.com>
2021-10-29 21:25:43 +0200

    Bump version -> v0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule StrongMigrations.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/patrykwozinski/strong_migrations"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
