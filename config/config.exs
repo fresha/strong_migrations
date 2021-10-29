621ff1e15b9986ca8a01c82e21f468810c1ec84b
Patryk Woziński <patryk.wozinski@gmail.com>
2021-10-29 16:12:00 +0200

    sample config

---
+++ config/config.exs
@@ -0,0 +1,19 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+# Sample configuration:
+#
+#     config :strong_migrations,
+#       migration_paths: [
+#         "priv/repo/migrations",
+#         "my/fancy/path/to/migrations"
+#       ],
+#       classifiers: [
+#         StrongMigrations.Classifiers.AddIndexConcurrentlyInTransaction,
+#         StrongMigrations.Classifiers.AddIndexNotConcurrently,
+#         StrongMigrations.Classifiers.DropIndexConcurrentlyInTransaction,
+#         StrongMigrations.Classifiers.DropTable,
+#         StrongMigrations.Classifiers.RemoveColumn,
+#         StrongMigrations.Classifiers.RenameColumn
+#       ]
